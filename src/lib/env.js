9e417239db02c3ff60f3d36ab57803856e85e9e2
VinhGiang731 <dinhvinhgiang345@gmail.com>
2025-11-18 11:28:38 +0700

    changed env.js

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,16 +1,5 @@
-const mode = import.meta.env.MODE;
-console.log("MODE:", mode);
-
-let BASE_BE_URL;
-
-if (mode === "development") {
-    BASE_BE_URL = import.meta.env.VITE_BASE_BE_URL;
-} else {
-    BASE_BE_URL = "https://be-danang-sakai.onrender.com/api/v1";
-}
-
 export const ENV = {
-    VITE_BASE_BE_URL: BASE_BE_URL,
+    VITE_BASE_BE_URL: 'https://be-danang-sakai.onrender.com/api/v1',
 };
 
 export default ENV;
